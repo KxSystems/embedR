8968e4da60457443c5bd114c6f2d692c20749c8f
Simon Shanks <sshanks@kx.com>
2026-02-23 16:28:01 +0000

    fix some compiler errors with gcc 9.3.0 / r 3.6.3

--- src/common.c
+++ src/common.c
@@ -225,7 +225,7 @@ static SEXP from_byte_kobject(K x)
 static K guid_2_char(G* x){
     K y= ktn(KC,37);
     G*gv= x;
-    sprintf(kC(y),"%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",gv[ 0],gv[ 1],gv[ 2],gv[ 3],gv[ 4],gv[ 5],gv[ 6],gv[ 7],gv[ 8],gv[ 9],gv[10],gv[11],gv[12],gv[13],gv[14],gv[15]);
+    sprintf((char*)kC(y),"%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",gv[ 0],gv[ 1],gv[ 2],gv[ 3],gv[ 4],gv[ 5],gv[ 6],gv[ 7],gv[ 8],gv[ 9],gv[10],gv[11],gv[12],gv[13],gv[14],gv[15]);
     y->n= 36;
     return(y);
 }
@@ -244,7 +244,7 @@ static SEXP from_guid_kobject(K x)
     }
     for(J i=0;i<x->n;i++){
       y= guid_2_char((G*)(&kU(x)[i]));
-      kK(z)[i]= kp(kC(y));
+      kK(z)[i]= kp((char*)kC(y));
       r0(y);
     }
     r = from_any_kobject(z);
